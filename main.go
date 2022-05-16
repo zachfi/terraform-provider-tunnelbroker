b05cc7d8013e63862c7eda8e446d0864c713fe0e
Zach Leslie <xaque208@gmail.com>
2022-05-16 22:46:15 +0000

    Include generated docs

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"github.com/xaque208/terraform-provider-tunnelbroker/tunnelbroker"
 )
 
+// Generate the Terraform provider documentation using `tfplugindocs`:
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
 func main() {
 	providerserver.Serve(context.Background(), tunnelbroker.New, providerserver.ServeOpts{
 		Address: "registry.terraform.io/xaque208/tunnelbroker",
